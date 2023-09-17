53b0b1e2db199a6b9a0f275528d29b2134b009b6
Miguel Avila <amiguelavila01@gmail.com>
2023-09-17 02:00:21 +0000

    set battery to report less than 50% and down

--- bash-script/battery_status.sh
+++ bash-script/battery_status.sh
@@ -7,7 +7,7 @@ source .env
 battery_status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep -o "[0-9]*")
 
 # Check if the battery percentage is less than 50%
-if [ "$battery_status" -lt 500 ]; then
+if [ "$battery_status" -lt 50 ]; then
    curl \
     -H "Tags: rotating_light, rotating_light, Low Battery, skull, warning" \
     -H "Title: Laptop Battery Low: $battery_status" \
